036dedd2e10a2fa35829a080eb0397b671b87e07
Valo Riutta <valo.riutta@gmail.com>
2025-04-23 13:06:02 +0300

    morgan

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+const morgan = require('morgan')
 app.use(morgan(':method :url :body'))
 app.use(cors())
 
